fd7a60fba905581e01c3cfd4caf96c4f059bc6e7
nacho2106 <inegri2106@gmail.com>
2026-05-08 18:07:02 -0300

    Add LED control functions and state updater

--- Laboratorio_2b/main/web/app.js
+++ Laboratorio_2b/main/web/app.js
@@ -1,1 +1,21 @@
+function encenderLED() {
+    fetch("/led/on")
+        .then(() => actualizarEstado());
+}
 
+function apagarLED() {
+    fetch("/led/off")
+        .then(() => actualizarEstado());
+}
+
+function actualizarEstado() {
+    fetch("/led")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("estado").innerText =
+                data.on ? "Encendido - " + data.color : "Apagado";
+        });
+}
+
+setInterval(actualizarEstado, 1000);
+actualizarEstado();
