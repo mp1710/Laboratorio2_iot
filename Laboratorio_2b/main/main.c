53ef0a98a75355cffbb62705a19664b108d64b8b
pilarmartinez <mariapilar.martinez@correo.ucu.edu.uy>
2026-05-09 14:07:41 -0300

    cambio main, agrego funciones del touchpad

--- Laboratorio_2b/main/main.c
+++ Laboratorio_2b/main/main.c
@@ -1,9 +1,12 @@
 #include "nvs_flash.h"
+#include "esp_err.h"
 
 #include "rgb_led.h"
 #include "wifi_manager.h"
 #include "web_server.h"
+#include "touch.h"
 
+// función principal de la aplicación
 void app_main(void)
 {
     esp_err_t ret = nvs_flash_init();
@@ -14,12 +17,17 @@ void app_main(void)
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-
+    // verificar que la inicialización de NVS fue exitosa
     ESP_ERROR_CHECK(ret);
-
+    // inicializar componentes
     rgb_led_init();
-
+    touchpad_init();
     wifi_init_softap();
-
     web_server_start();
+
+    // loop infinito para mantener la aplicación corriendo
+    while (1)
+    {
+        touchpad_update_led();
+    }
 }
