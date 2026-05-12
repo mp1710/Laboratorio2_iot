3de3c07c479c8a15fed43e709c042b386e459c24
pilarmartinez <mariapilar.martinez@correo.ucu.edu.uy>
2026-05-12 11:15:55 -0300

    main con web/

---
+++ Laboratorio_2b/main/web/app.js
@@ -0,0 +1,28 @@
+function setColor(color)
+{
+    fetch(`/led/${color}`)
+        .then(() => actualizarEstado());
+}
+
+function apagarLED()
+{
+    fetch("/led/off")
+        .then(() => actualizarEstado());
+}
+
+function actualizarEstado()
+{
+    fetch("/led")
+        .then(response => response.json())
+        .then(data => {
+
+            document.getElementById("estado").innerText =
+                data.on
+                    ? "Encendido - " + data.color
+                    : "Apagado";
+        });
+}
+
+setInterval(actualizarEstado, 1000);
+
+actualizarEstado();
