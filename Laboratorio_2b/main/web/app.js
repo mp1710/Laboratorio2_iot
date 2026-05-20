e135ff281ffceaeee633ec815282953a08bbffdb
nacho2106 <inegri2106@gmail.com>
2026-05-19 22:48:56 -0300

    Change fetch method to POST for setColor and apagarLED

--- Laboratorio_2b/main/web/app.js
+++ Laboratorio_2b/main/web/app.js
@@ -1,28 +1,25 @@
-function setColor(color)
-{
-    fetch(`/led/${color}`)
-        .then(() => actualizarEstado());
+function setColor(color) {
+    fetch(`/led/${color}`, {
+        method: "POST"
+    })
+    .then(() => actualizarEstado());
 }
 
-function apagarLED()
-{
-    fetch("/led/off")
-        .then(() => actualizarEstado());
+function apagarLED() {
+    fetch("/led/off", {
+        method: "POST"
+    })
+    .then(() => actualizarEstado());
 }
 
-function actualizarEstado()
-{
+function actualizarEstado() {
     fetch("/led")
         .then(response => response.json())
         .then(data => {
-
             document.getElementById("estado").innerText =
-                data.on
-                    ? "Encendido - " + data.color
-                    : "Apagado";
+                data.on ? "Encendido - " + data.color : "Apagado";
         });
 }
 
 setInterval(actualizarEstado, 1000);
-
 actualizarEstado();
